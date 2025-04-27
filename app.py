2a7f5ac29c44fc29403cca031a86c8e04aa8bb60
kobtair <aaqibimran0@gmail.com>
2025-04-27 16:56:37 +0500

    Hello World

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ CORS(app)
 
 app.register_blueprint(routes)
 
+@app.route("/")
+def hello_world():
+    return "Hello, World!"
+
 port = int(os.environ.get("PORT", 8000))
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=port)
