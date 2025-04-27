f928fe530d6fb35fc7d5ebda0f7b4157ee77623e
kobtair <aaqibimran0@gmail.com>
2025-04-27 15:27:48 +0500

    port change

--- app.py
+++ app.py
@@ -9,6 +9,6 @@ CORS(app)
 
 app.register_blueprint(routes)
 
-port = int(os.environ.get("PORT", 5000))
+port = int(os.environ.get("PORT", 8000))
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=port)
