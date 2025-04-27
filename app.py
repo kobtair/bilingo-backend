d71bcea50b0e9f77725843bbea6c815400a273a4
kobtair <aaqibimran0@gmail.com>
2025-04-27 14:58:40 +0500

    Initial commit

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from routes import routes
 from flask_cors import CORS
 
 app = Flask(__name__)
-app.config["DEBUG"] = True  # Only include this while you are testing your app
+# app.config["DEBUG"] = True  # Only include this while you are testing your app
 CORS(app)
 
 app.register_blueprint(routes)
